2a170f1f3068eef3ccf0cda46b196b8f9528c26c
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-07-13 11:43:20 +0530

    improve the redirect message

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ module.exports = {
           '<link rel="canonical" href="' +  path + '">\n' +
           '<meta http-equiv=refresh content="0; url=' + encodeURI(path) + '">\n' +
           '<h1>Redirecting...</h1>\n' +
-          '<a href="' + path + '">Click here if you are not redirected.</a>\n' +
+          '<p>\n' +
+          '  This page has moved to \n' +
+          '  <a href="' + path + '">' + path +'</a>.\n' +
+          '</p>\n' +
+          '<p>\n' +
+          '  <a href="' + path + '">Click here</a> if you are not redirected.\n' +
+          '</p>\n' +
           '<script>window.location.href="' + path + '";</script>\n';
       };
 
